3e12ea317bc404adb745ad32568085f20b35cf2d
Cole Styron <colestyron@gmail.com>
2023-03-17 14:17:52 -0800

    adds new launch validation

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,9 +1,35 @@
-const { getAllLaunches } = require("../../models/launches.model");
+const { addNewLaunch, getAllLaunches } = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
-  return res.status(200).json(Array.from(getAllLaunches.values()));
+  return res.status(200).json(Array.from(getAllLaunches()));
+}
+
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({
+      error: "missing required launch properties",
+    });
+  }
+
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "inavlid launch date format",
+    });
+  }
+
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
 }
 
 module.exports = {
   httpGetAllLaunches,
+  httpAddNewLaunch,
 };
