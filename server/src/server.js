e3bc9b7468a2a29ca86c4cc7fc31baf0aae6bfc2
Cole Styron <colestyron@gmail.com>
2023-03-21 13:35:16 -0800

    adds mongodb integration

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,7 @@
 const http = require("http");
+const mongoose = require("mongoose");
+require('dotenv').config()
+const uri = process.env.MONGO_URL
 
 const app = require("./app");
 
@@ -8,7 +11,17 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection established!");
+});
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+
 async function startServer() {
+  await mongoose.connect(uri);
+
   await loadPlanetsData();
 
   server.listen(PORT, () => {
